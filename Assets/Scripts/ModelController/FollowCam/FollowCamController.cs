c4b9fc2ed740ec0323736dadd716b875238873bd
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-08-03 20:35:22 +0600

    Added Waterrrr and Cam movement

---
+++ Assets/Scripts/ModelController/FollowCam/FollowCamController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ModelController.FollowCam
+{
+    public class FollowCamController : MonoBehaviour
+    {
+        public GameObject target;
+        public Vector3 starting;
+
+        void Start()
+        {
+            starting = transform.position;
+        }
+
+        private void Update()
+        {
+            Vector3 targetPosition = target.transform.position + starting; 
+            transform.position = targetPosition;
+        }
+    }
+}
